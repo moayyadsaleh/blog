0581750aaf02540fa70aa97be05eacb511e83b50
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-01 18:06:50 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import axios from "axios";
+
+const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+const aboutContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here";
+const contactContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. ";
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('home', {StartingContent: homeStartingContent});
+});
+
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
